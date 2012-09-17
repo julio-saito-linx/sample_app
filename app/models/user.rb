66da90dcda23bb4af4089bdf11ba5c867722db1e
Julio Saito <saitodisse@gmail.com>
2012-09-17 14:50:46 -0300

    regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name
 
   validates :name, presence: true, length: {maximum: 50}
-  validates :email, presence: true
+
+  VALID_EMAIL_REGEX = /^[\w+\-.]+@[a-z\d\-.]+\.\w+$/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
